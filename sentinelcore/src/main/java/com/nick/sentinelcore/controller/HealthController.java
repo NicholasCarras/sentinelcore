d295c57f76d3422cf0f1f0749838c0c01df21fa9
Nicholation <carrasquilla.nicholas@gmail.com>
2026-05-09 09:21:15 -0400

    Added code documentation for readability

--- sentinelcore/src/main/java/com/nick/sentinelcore/controller/HealthController.java
+++ sentinelcore/src/main/java/com/nick/sentinelcore/controller/HealthController.java
@@ -3,8 +3,10 @@ package com.nick.sentinelcore.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // Lets Spring Boot know this class is for APIs
 public class HealthController {
+
+    // Creates endpoint
 
     @GetMapping("/health")
     public String health() {
--- sentinelcore/src/main/java/com/nick/sentinelcore/model/Node.java
+++ sentinelcore/src/main/java/com/nick/sentinelcore/model/Node.java
@@ -2,15 +2,21 @@ package com.nick.sentinelcore.model;
 
 public class Node {
 
+    // Attributes
+
     private int id;
     private String name;
     private String status;
+
+    // Constructor for node
 
     public Node(int id, String name, String status) {
         this.id = id;
         this.name = name;
         this.status = status;
     }
+
+    // Getters
 
     public int getId() {
         return id;
